1c74ad5cd2674a28b8fac024a61adb19b78d6157
Mohamed Ijas M A <ijas14624@gmail.com>
2025-08-01 11:54:16 +0530

    Time: 12 ms (97.27%), Space: 46.3 MB (94.4%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+         Arrays.sort(nums);
+       int maxSum =0;
+
+       for(int i=0;i<nums.length;i=i+2){
+        maxSum = maxSum+nums[i];
+       } 
+      return maxSum;
+    }
+}
