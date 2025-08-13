8a818359d1a7acbeca83e63f569311b54dd3b02e
Mohamed Ijas M A <ijas14624@gmail.com>
2025-08-13 10:26:52 +0530

    Time: 4 ms (100%), Space: 44.5 MB (11.02%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int temp=0;
+        int num=x;
+        while(num!=0){
+            temp = temp*10 + num%10;
+            num=num/10;
+        }
+        return (temp==x);
+    }
+}
